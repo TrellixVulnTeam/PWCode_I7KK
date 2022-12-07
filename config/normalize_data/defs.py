a7b75ae6a53324caf87976cf7604666df63ac550
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-07 17:06:48 +0000

    Adding tarfile member sanitization to extractall()

--- config/normalize_data/defs.py
+++ config/normalize_data/defs.py
@@ -347,7 +347,26 @@ def normalize_data(project_dir, bin_dir, class_path, memory, tmp_dir, convert):
                     subprocess.run('wimmount ' + str(file) + ' ' + mount_dir, shell=True)
                 else:
                     with tarfile.open(file) as tar:
-                        tar.extractall(path=export_dir)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar, path=export_dir)
 
                 run_tika(tsv_file, mount_dir, tika_tmp_dir)
 
